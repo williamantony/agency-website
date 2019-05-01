3bd41f6079e46ee424ab414d383b6bdea176db8a
William Antony <william@willsquad.com>
2019-05-01 16:42:21 -0500

    Added react-router-dom; Implemented React-router in App & index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-class App extends Component {
+/* Error Pages */
+import Landing from './pages/Landing/Landing';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 
-  render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
-  }
-
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Landing} />
+    <Route component={ErrorPage} />
+  </Switch>
+);
 
 export default App;
