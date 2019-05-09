a05418deda7295a9e2abbfd1587b51da28fd232f
William Antony <william@willsquad.com>
2019-05-08 22:34:00 -0500

    Initialized Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import Reducers from './redux/reducers';
 import App from './App';
 
+const store = createStore(Reducers, applyMiddleware(ReduxThunk));
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
