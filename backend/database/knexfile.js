9983a6497ce1c823575d8bf4d709bd6598068255
William Antony <william@willsquad.com>
2019-05-23 21:54:48 -0500

    Created Knexfile and configuration

---
+++ backend/database/knexfile.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: path.join(__dirname, '../', '.env'),
+});
+
+const {
+  MODE,
+  DEV_DB_HOST,
+  DEV_DB_USER,
+  DEV_DB_PASS,
+  DEV_DB_NAME,
+  PROD_DB_HOST,
+  PROD_DB_USER,
+  PROD_DB_PASS,
+  PROD_DB_NAME,
+} = process.env;
+
+const config = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host : DEV_DB_HOST,
+      user : DEV_DB_USER,
+      password : DEV_DB_PASS,
+      database : DEV_DB_NAME,
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'mysql',
+    connection: {
+      host : PROD_DB_HOST,
+      user : PROD_DB_USER,
+      password : PROD_DB_PASS,
+      database : PROD_DB_NAME,
+    },
+    useNullAsDefault: true
+  },
+};
+
+module.exports = MODE === 'DEV' ? config.development : config.production;
